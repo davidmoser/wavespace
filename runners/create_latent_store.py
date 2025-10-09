4bcfbc1368a93fb86c870558557a0567daf13a50
David <david.moser@gmx.net>
2025-10-09 12:46:50 +0200

    Remove latent event normalization helper

--- runners/create_latent_store.py
+++ runners/create_latent_store.py
@@ -1,23 +1,26 @@
 from datasets.create_latent_store import create_latent_store
-from datasets.sine_dataset import SineDataset
+from datasets.poly_dataset import PolyphonicAsyncDataset
 
-duration = 1
+duration = 1.0
 sampling_rate = 24000
-min_freq = 100
-max_freq = 10000
+min_freq = 100.0
+max_freq = 10000.0
 num_samples = 10000
+max_polyphony = 4
+min_note_duration = 0.12
 
-dataset = SineDataset(
+dataset = PolyphonicAsyncDataset(
+    n_samples=num_samples,
+    freq_range=(min_freq, max_freq),
+    max_polyphony=max_polyphony,
+    sr=sampling_rate,
     duration=duration,
-    sampling_rate=sampling_rate,
-    min_frequency=min_freq,
-    max_frequency=max_freq,
-    num_samples=num_samples,
+    min_note_duration=min_note_duration,
 )
 
 create_latent_store(
     dataset,
-    dataset_path=f"../resources/encodec_latents/sines_1",
+    dataset_path="../resources/encodec_latents/poly_async",
     dataset_sample_rate=sampling_rate,
     metadata={
         "dataset": dataset.__class__.__name__,
@@ -27,6 +30,8 @@ create_latent_store(
             "min_frequency": min_freq,
             "max_frequency": max_freq,
             "num_samples": num_samples,
+            "max_polyphony": max_polyphony,
+            "min_note_duration": min_note_duration,
         },
     },
     # sample_rate: Optional[int] = None,
