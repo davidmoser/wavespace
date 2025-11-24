c69c8b03c49a6b4fcb66f3ff6478063a7033d56b
David Moser <david.moser@gmx.net>
2025-11-24 12:13:29 +0100

    refactor to plot_cqt.py

--- jobs/samples/plot_cqt.py
+++ jobs/samples/plot_cqt.py
@@ -3,11 +3,11 @@ import librosa
 from utils.plot_cqt_comparison import plot_cqt
 
 plot_cqt(
-    audio_file="../../resources/maestro-v3.0.0/2004/MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav",
-    output_image="../../resources/maestro-v3.0.0/samples/MIDI-Unprocessed_SMF_02_Track05_wav_cqt_high.png",
+    audio_file="../../resources/Gentle on My Mind - Cotton Pickin Kids/Gentle on My Mind - Cotton Pickin Kids.mp3",
+    output_image="../../resources/Gentle on My Mind - Cotton Pickin Kids/all_cqt_512.png",
     duration_seconds=20,
     # scale_values=scale_values,
-    cqt_bins=256,
+    cqt_bins=512,
     cqt_bins_per_octave=24,
     cqt_frames_per_s=20,
     cqt_fmin=float(librosa.midi_to_hz(0)),
